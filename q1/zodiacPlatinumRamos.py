51dab4d7f27a6e763ce37a468d4ccfcaeb1e2b25
jasdari ramos <jvbramos@brc.pshs.edu.ph>
2026-08-13 06:48:30 +0800

    Update zodiacPlatinumRamos.py

--- q1/zodiacPlatinumRamos.py
+++ q1/zodiacPlatinumRamos.py
@@ -1,3 +1,36 @@
+## Instructions
+Create a zodiacSectionLN.py file.  This file will contain your solutions to the requirements below:
+
+a. Ask the user to enter a year of birth.  The baseline year 1900.
+b. Validate user input that it should not be earlier than 1900.
+c. If the user enters an invalid year then display an appropriate message then stop or abort the program.
+
+Example:
+Enter your birth year: 1800
+Invalid Year, it should not be earlier than 1900
+
+d. Otherwise determine the chinese zodiac sign based on the following starting from 1900.  Note: A zodiac sign will recur after each 12 years.
+
+i. Rat (鼠 / Shǔ)
+ii. Ox (牛 / Niú)
+iii. Tiger (虎 / Hǔ)
+iv. Rabbit (兔 / Tù)
+v. Dragon (龙 / Lóng)
+vi. Snake (蛇 / Shé)
+vii. Horse (马 / Mǎ)
+viii. Goat (羊 / Yáng)
+ix. Monkey (猴 / Hóu)
+x. Rooster (鸡 / Jī)
+xi. Dog (狗 / Gǒu)
+xii. Pig (猪 / Zhū)
+
+e. CONSIDER only the year of birth.
+
+Example input and output:
+Enter your birth year: 2000
+Your Chinese Zodiac Sign is: Dragon (龙 / Lóng)
+
+## Code
 a = int(input("Enter your birth year: "))
 
 if a < 1900:
@@ -28,3 +61,4 @@ else:
     print("Dog (狗 / Gǒu)")
   elif b == 11:
     print("Pig (猪 / Zhū)")
+
