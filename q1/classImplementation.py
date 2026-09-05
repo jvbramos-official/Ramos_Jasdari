5ab8d545f7be2cc01c0a3901dd24d8e77dfb9db2
jasdari ramos <jvbramos@brc.pshs.edu.ph>
2026-09-05 16:02:00 +0800

    Update classImplementation.py

--- q1/classImplementation.py
+++ q1/classImplementation.py
@@ -1,5 +1,5 @@
 class Plant:
   def __init__(self, name, age, color, height):
   self.name = value1
-  self.attribute2 = value2
-  self.__private_attribute = value3
+  self.age = value2
+  self.__private_colorz = value3
