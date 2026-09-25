5c20eeaaa3543a76602cc531d0c13c46b6d94322
jasdari ramos <jvbramos@brc.pshs.edu.ph>
2026-09-25 14:45:33 +0800

    Update advancedRelationships.py

--- q1/advancedRelationships.py
+++ q1/advancedRelationships.py
@@ -1,10 +1,10 @@
+
 class Plant:
     def __init__(self, name, age, color, height):
         self.Name = name
         self.Age = age
         self.Color = color
         self.__private_Height = height
-        
     
     def Reproduce(self):
         baby_plant = f"baby {self.Name}"
@@ -19,20 +19,49 @@ class Plant:
     
     def getHeight(self):
         return self.__private_Height
-
-class Flower:
-    def __init__(self, name, age, color, fragrance):
-        super().__init__(name, age, color)
-        self.Fragrance = fragrance
     
-    def Bloomed_or_not(self):
-        if self.Age >= 60:
-            print("The flower has bloomed.")
+Plant1 = Plant("Basil", 2, "Green", 5)
+
+print("Parent Attribute:")
+print(f"Height = {Plant1.getHeight()}")
+print("-")
+
+class Stamen:
+    def __init__(self, countpollen=676):
+        self.PollenCount = countpollen
+
+    def ReleasePollen(self):
+        if self.PollenCount > 0:
+            self.PollenCount -= 367
+            return "= Pollen is everywhere now. ="
         else:
-            print("The flower has not bloomed yet.")
+            return "No more pollen left :c."
+
+
+class Flower(Plant):
+    def __init__(self, name, age, color, height, fragrance):
+        super().__init__(name, age, color, height)
+        self.Fragrance = fragrance
+        
+        self.stamen_part = Stamen(countpollen=676)
         
     def Grow(self, days):
         super().Grow(days)
-        
         self.Age += days
         print(f"{self.Name} grew for {days} days. The age is now {self.Age}.")
+
+
+Flower1 = Flower("Rose", 4, "Red", 10, "Damask")
+
+print("Child Object:")
+print(f"Name: {Flower1.Name}")
+print(f"Height: {Flower1.getHeight()}")
+print("-")
+
+print(f"Object: {Flower1.Name}")
+print(f"Pollen Count before: {Flower1.stamen_part.PollenCount}")
+    
+spread = Flower1.stamen_part.ReleasePollen()
+print(spread)
+    
+print(f"Pollen Count now: {Flower1.stamen_part.PollenCount}")
